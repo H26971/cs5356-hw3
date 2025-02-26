ac1b15b628c42ce449731dc5cbd975043f247f64
hanqi <your email>
2025-02-26 15:23:28 -0500

    material change

--- js/mouse-tracker.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("mousemove", (event) => {
-    let x = event.clientX / window.innerWidth;
-    let y = event.clientY / window.innerHeight;
-    let red = Math.round(x * 255);
-    let green = Math.round(y * 255);
-    let blue = Math.round((1 - x) * 255);
-
-    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-});
